10bcd80661be93b7d832c0cd462ddd299e86acf2
RavanBayramov1 <ravanvb-ab108@code.edu.az>
2024-11-21 03:48:08 +0400

    Uniqlo first step added

---
+++ Uniqlo_1/Uniqlo_1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Uniqlo_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
